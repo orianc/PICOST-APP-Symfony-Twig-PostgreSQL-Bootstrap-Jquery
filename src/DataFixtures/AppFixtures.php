e7657b3323ce6a43de8b769dbdbceebdbb20ed3a
orianc <34635176+orianc@users.noreply.github.com>
2021-05-05 13:00:08 +0200

    Mise en place des crud et début des templates

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,41 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use App\Entity\Album;
+use App\Entity\Photo;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $album = [
+            'Portrait',
+            'Landscade',
+            'Event'
+        ];
+
+        foreach ($album as $a){
+            $album = new Album;
+            $album
+                ->setName($a)
+                ->setCreateAt(new DateTime('now'))
+                ->setUpdateAt(new DateTime('now'))
+                ;
+            
+            $albumArrayObj[] = $album;
+            $manager->persist($album);
+        }
+        for ($i=0; $i < 10; $i++) { 
+            $photoPortrait = new Photo;
+            $photoPortrait
+                ->setName('pexels-photo-' . $i .  '.jpeg')
+                ->setIntoAlbum($albumArrayObj[0])
+            ;
+            $manager->persist($photoPortrait);
+        }
 
         $manager->flush();
     }
